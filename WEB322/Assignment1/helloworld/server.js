100e85a3b1c2c33595196cd77cd5d6d0da642a51
zzhao58 <zoeyzhao100@gmail.com>
2019-01-07 23:51:34 -0500

    change name and student ID

--- WEB322/Assignment1/helloworld/server.js
+++ WEB322/Assignment1/helloworld/server.js
@@ -1,3 +1,13 @@
+/*********************************************************************************
+* WEB322 – Assignment 1
+* I declare that this assignment is my own work in accordance with Seneca Academic Policy.
+* No part of this assignment has been copied manually or electronically from any other source
+* (including web sites) or distributed to other students.
+*
+* Name: Zhi__Zhao____________________ Student ID: __109079178____________ Date: ___1/7/2019_____________
+*
+* Online (Heroku) URL: __https://frozen-anchorage-53217.herokuapp.com/
+********************************************************************************/ 
 var HTTP_PORT = process.env.PORT || 8080;
 var express = require("express");
 var app = express();
@@ -8,4 +18,4 @@ app.get("/", (req, res) => {
 });
 
 // setup http server to listen on HTTP_PORT
-app.listen(HTTP_PORT);
+app.listen(HTTP_PORT); 
