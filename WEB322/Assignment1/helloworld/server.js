24e4d5478eb78deaaa3eb2c71757ae5bf8503ca4
zzhao58 <zoeyzhao100@gmail.com>
2019-01-07 23:45:59 -0500

    second commit

--- WEB322/Assignment1/helloworld/server.js
+++ WEB322/Assignment1/helloworld/server.js
@@ -4,7 +4,7 @@ var app = express();
 
 // setup a 'route' to listen on the default url path
 app.get("/", (req, res) => {
-    res.send("Hello World!");
+    res.send("Zhi Zhao-109079178");
 });
 
 // setup http server to listen on HTTP_PORT
